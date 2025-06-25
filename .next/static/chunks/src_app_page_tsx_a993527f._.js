90dd47c4d7a3182213a72611bb7f1d681aaddf24
ivansantoscoy <ivan.msantoscoy@gmail.com>
2025-06-25 15:07:56 -0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750889276242

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d13ec29e._.js",
+  "static/chunks/node_modules_xlsx_xlsx_mjs_ad755052._.js",
+  "static/chunks/node_modules_next_dist_compiled_f45921cb._.js"
+],
+    source: "dynamic"
+});
